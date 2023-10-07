a8901ec2cda7b38d1e71680bed6f325eee65d226
João Victor Tenório <jvtenorio7@gmail.com>
2023-10-07 17:26:18 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Componentes/Home';
+import Sobre from "./Componentes/Sobre";
+import Trabalho from './Componentes/Trabalho';
+import Depoimentos from './Componentes/Depoimentos';
+import Contatos from './Componentes/Contatos';
+import Footer from './Componentes/Footer';
+import Formulario from './Componentes/Formulario';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
+      <Sobre/>
+      <Trabalho/>
+      <Depoimentos/>
+      <Formulario/>
+      <Contatos/>
+      <Footer/>
     </div>
   );
 }
